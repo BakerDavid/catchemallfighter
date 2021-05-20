e6732455cd6db4962ec35df5c6a178c4de93ca63
David <bakermdavid@gmail.com>
2021-05-20 01:45:46 -0700

    Timer between attacks, button disable after someone dies, health bar, health text, win lose message.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 var player = {
-	hp: 100,
+	hp: "100",
 	max_hp: 100,
 	smack:  [4,6,8],
 	defence: 1
 };
 
 var target = {
-	hp: 100,
+	hp: "100",
 	max_hp:100,
 	defence:1,
 	stare: [4,6,8]
-}
+};
 
 var targethealth = document.getElementById("targethealth");
 
@@ -26,16 +26,42 @@ function targetattack(input) {
 	return player.hp - (randam - player.defence);
 };
 
-document.getElementById("attack").onclick = function(){
-    target.hp = playerattack(player.smack);   
-    targethealth.value = target.hp
-    setTimeout(function() {player.hp = targetattack(target.stare), playerhealth.value = player.hp; }, 800);
-    
+document.getElementById("attack").onclick = function(){   
+        setTimeout(updateValues, 00);
+        setTimeout(updateValues1, 600);
+    	document.getElementById("attack").disabled = true;
+    setTimeout(function(){document.getElementById("attack").disabled = false;},800);
+    setTimeout(function(){if(player.hp, target.hp <=0) {
+    	document.getElementById("attack").disabled = true;
+    	}
+	}, 802)
+};
 
-    if(player.hp <= 0) {
+function updateValues() { 
+	target.hp = playerattack(player.smack); 
+	targethealth.value = target.hp; 
+	playerhealth.value = player.hp; 
+	document.getElementById("displayer").innerHTML = player.hp; 
+	document.getElementById("distarget").innerHTML = target.hp;
+	    if(player.hp <= 0) {
     	document.getElementById("endmessage").innerHTML="You have died!";
     } 
-    if(target.hp <= 0) {
+    	if(target.hp <= 0) {
     	document.getElementById("endmessage").innerHTML="You won!";
-    }
-};
+    }	
+}
+function updateValues1() {
+	if(target.hp > 0){
+		player.hp = targetattack(target.stare);  
+		targethealth.value = target.hp; 
+	}
+	playerhealth.value = player.hp; 
+	document.getElementById("displayer").innerHTML = player.hp; 
+	document.getElementById("distarget").innerHTML = target.hp;
+	    if(player.hp <= 0) {
+    	document.getElementById("endmessage").innerHTML="You have died!";
+    } 
+    	if(target.hp <= 0) {
+    	document.getElementById("endmessage").innerHTML="You won!";
+    }	
+}
